bc968d98fe7612d0ff08fae726af151f6cd43361
jeffersonnobrega <jeffersonnobrega@gmail.com>
2026-02-18 11:37:42 -0300

    chore: ajustes antes do rebase

---
+++ Dotnet/SistemaAtendimento/Atendimento.Domain/Entities/Cliente.cs
@@ -0,0 +1,13 @@
+
+namespace Atendimento.Domain.Entities;
+
+public class Cliente
+{
+    public int Id { get; set; }
+
+    public string Matricula { get; set; }
+    public string Nome { get; set;}
+
+    public int? DepartamentoId { get; set; }
+    public Departamentos? Departamento { get; set; }
+}
--- Dotnet/SistemaAtendimento/Atendimento.Domain/Entities/Departamentos.cs
+++ Dotnet/SistemaAtendimento/Atendimento.Domain/Entities/Departamentos.cs
@@ -2,5 +2,9 @@ namespace Atendimento.Domain.Entities;
 
 public class Departamentos
 {
+    public int Id { get; set; }
+
+    public string Nome { get; set; }
+    public string? CodDepartamento { get; set; } = string.Empty;
     
 }
